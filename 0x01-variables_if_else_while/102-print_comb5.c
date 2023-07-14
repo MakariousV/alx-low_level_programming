56eeda89e22d1224cf88b869f2a4937fd34ab5d5
MakariousV <makarusvictor@gmail.com>
2023-07-14 07:26:35 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+			    putchar(i%10; j)
+			}
+		}
+	}
+		putchar('\n');
+	return (0);
+}
